fa3c7f0e656c5db86aa17b6296a9457cc2806bc7
m-24kawa <zhengyuxichuan@gmail.com>
2025-05-11 16:09:06 +0900

    Timer application revision 4.0
    lap mode added. (cont to cancel)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 'use strict';
 {
+  const urlParams = new URLSearchParams(window.location.search);
+  let cvBackGround = urlParams.get('back');
+  let cvForeGround = urlParams.get('fore');
+  if (cvBackGround==null || cvForeGround==null){
+    cvBackGround = '#66c';  // 背景色
+    cvForeGround = '#ddd';  // 文字色
+  }
+  function InitScreen(){
+    // 画面背景初期化
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === 'undefined') {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(canvas.width,0);
+    ctx.lineTo(canvas.width,canvas.height);
+    ctx.lineTo(0,canvas.height);
+    ctx.lineTo.closePath;
+    ctx.fillStyle = cvBackGround;
+    ctx.fill();
+  }
   function clearStr(tgtStr,ix,iy){
     // 文字列削除処理（一括）
     const canvas = document.querySelector('canvas');
@@ -14,7 +37,7 @@
     ctx.lineTo(ix+(tgtStr.length)*45-7, iy+75);
     ctx.lineTo(ix-9, iy+75);
     ctx.lineTo.closePath;
-    ctx.fillStyle = '#aaa';
+    ctx.fillStyle = cvBackGround;
     ctx.fill();
   }
   function putStr2(tgtStr,ix,iy) {
@@ -31,7 +54,7 @@
     ctx.lineTo(ix+(tgtStr.length)*45-7, iy+75);
     ctx.lineTo(ix-9, iy+75);
     ctx.lineTo.closePath;
-    ctx.fillStyle = '#aaa';
+    ctx.fillStyle = cvBackGround;
     ctx.fill();
 
     // 文字列描画処理
@@ -62,7 +85,7 @@
     ctx.lineTo(x+4,y+74);
     ctx.lineTo(x-6,y+74);
     ctx.lineTo.closePath;
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = cvForeGround;
     ctx.fill();
   }
   // Colonの描画
@@ -73,7 +96,7 @@
     ctx.lineTo(x+23,y+26);
     ctx.lineTo(x+13,y+26);
     ctx.lineTo.closePath;
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = cvForeGround;
     ctx.fill();
     ctx.beginPath();
     ctx.moveTo(x+11,y+48);
@@ -81,7 +104,7 @@
     ctx.lineTo(x+20,y+58);
     ctx.lineTo(x+10,y+58);
     ctx.lineTo.closePath;
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = cvForeGround;
     ctx.fill();
   }
   // マイナスの描画
@@ -94,7 +117,7 @@
     ctx.lineTo(x+27,y+42);
     ctx.lineTo(x+7,y+42);
     ctx.lineTo.closePath;
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = cvForeGround;
     ctx.fill();
   }
 
@@ -122,7 +145,7 @@
       ctx.lineTo(ix+SegOffsetX[1]+25,iy+SegOffsetY[1]+5);
       ctx.lineTo(ix+SegOffsetX[1]+5,iy+SegOffsetY[1]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     }
     //セグメント２作成
@@ -135,7 +158,7 @@
       ctx.lineTo(ix+SegOffsetX[2]-7,iy+SegOffsetY[2]+25);
       ctx.lineTo(ix+SegOffsetX[2]-5,iy+SegOffsetY[2]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     } 
     //セグメント３作成
@@ -149,7 +172,7 @@
       ctx.lineTo(ix+SegOffsetX[3]-7,iy+SegOffsetY[3]+25);
       ctx.lineTo(ix+SegOffsetX[3]-5,iy+SegOffsetY[3]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     } 
     //セグメント４作成
@@ -162,7 +185,7 @@
       ctx.lineTo(ix+SegOffsetX[4]+25,iy+SegOffsetY[4]+5);
       ctx.lineTo(ix+SegOffsetX[4]+5,iy+SegOffsetY[4]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     } 
     //セグメント５作成
@@ -175,7 +198,7 @@
       ctx.lineTo(ix+SegOffsetX[5]-7,iy+SegOffsetY[5]+25);
       ctx.lineTo(ix+SegOffsetX[5]-5,iy+SegOffsetY[5]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     } 
     //セグメント６作成
@@ -189,7 +212,7 @@
       ctx.lineTo(ix+SegOffsetX[6]-7,iy+SegOffsetY[6]+25);
       ctx.lineTo(ix+SegOffsetX[6]-5,iy+SegOffsetY[6]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     } 
     //セグメント７作成
@@ -202,7 +225,7 @@
       ctx.lineTo(ix+SegOffsetX[7]+25,iy+SegOffsetY[7]+5);
       ctx.lineTo(ix+SegOffsetX[7]+5,iy+SegOffsetY[7]+5);
       ctx.lineTo.closePath;
-      ctx.fillStyle = '#000000';
+      ctx.fillStyle = cvForeGround;
       ctx.fill();
     }
   }
@@ -221,77 +244,58 @@
     const sec = String(Math.floor(t_sc / 1000 )).padStart(2,'0');
     const t_c = t_sc % 1000;
     const csec = String(Math.floor(t_c/10)).padStart(2,'0');
-    strTime = hour + ':' + min + ':' + sec + '.' +csec;
+    var strTime = hour + ':' + min + ':' + sec + '.' +csec;
     putStr2(strTime,25,50);
   }
 
   // ボタン要素取得(表示)
   const buttonElement_start = document.getElementById('btn_start');
-  const buttonElement_reset = document.getElementById('btn_reset');
+  const buttonElement_reset = document.getElementById('btn_reset');      
   //  ボタン要素取得(非表示)
   const buttonElement_stop = document.getElementById('btn_stop');
   buttonElement_stop.style.display = 'none';
   const buttonElement_lap = document.getElementById('btn_lap');
   buttonElement_lap.style.display = 'none';
-  const buttonElement_exit = document.getElementById('btn_exit');
-  buttonElement_exit.style.display = 'none';
-  const buttonElement_save_exit = document.getElementById('btn_save_exit');
-  buttonElement_save_exit.style.display = 'none';
+  const buttonElement_lapstar = document.getElementById('btn_lapstar');
+  buttonElement_lapstar.style.display = 'none';
+  const buttonElement_cont = document.getElementById('btn_cont');
+  buttonElement_cont.style.display = 'none';
   //const buttonElement = document.querySelector('button');
   let tBase = 0; 
   let tDiff = 0;
-  let prevDiff = 0; 
-  let tOffset = 0;
-  var strTime;
+  let tOffset = 0;    // 累積更新時間
+  let tPrevDiff = 0;  // LAP押下時のタイム
+  let tLapBase = 0;   // Lap基準タイム 
+  InitScreen();
   putTime(tOffset);
 
   var timerId;
-  let Lap_flg = 0; // LAP可否フラグ
-  let Lap_on = 0; // LAP処理中(使用時は初期値1)
+  let Ud_flg =0; // タイマ表示・非表示
+  //let Lap_flg = 1; // LAP可否フラグ
+  let Lap_on = 0; // LAPモード設定(未設定:0、設定中:1)
+  let Set_on = 0; // LAP基準タイム設定
 
   buttonElement_start.addEventListener('click',() => {
     // console.log(' tOffset  :',tOffset);
     // console.log(' tDiff    :',tDiff);
-    tBase = Date.now();//UTC time 
+    Ud_flg =1; // 表示ON
+    tBase = Date.now();//UTC time
+
     timerId=setInterval(()=>{
       tDiff = Date.now() - tBase;
-      // ボタン表示切替
-      buttonElement_start.style.display = 'none';
-      buttonElement_reset.style.display = 'none';
-      buttonElement_stop.style.display = 'inline-block';
-      if (Lap_flg){
-          buttonElement_lap.style.display = 'inline-block';
+      // ボタン表示 (Lapモードのときはスキップ)
+      if (Lap_on==0) {
+        buttonElement_start.style.display = 'none';
+        buttonElement_reset.style.display = 'none';
+        buttonElement_stop.style.display = 'inline-block';
+        if (Set_on==1) {      
+            buttonElement_lapstar.style.display = 'inline-block';
+        } else {
+            buttonElement_lap.style.display = 'inline-block';
+        }
       }
-     
-      // STOPボタン押下処理
-      buttonElement_stop.addEventListener('click',()=>{
-        // 繰り返し処理終了
-        clearInterval(timerId);
-        // LAPは不可
-        Lap_flg =0;
-        // ボタン表示切替
-        buttonElement_stop.style.display = 'none';
-        buttonElement_lap.style.display = 'none';
-        buttonElement_start.style.display = 'inline-block';
-        buttonElement_reset.style.display = 'inline-block';
-        return;
-      });
-      // LAPボタン
-      buttonElement_lap.addEventListener('click',()=>{
-        // 繰り返し処理終了
-        clearInterval(timerId);
-        // ボタン表示切替
-        buttonElement_stop.style.display = 'none';
-        buttonElement_lap.style.display = 'none';
-        buttonElement_exit.style.display = 'inline-block';
-        buttonElement_save_exit.style.display = 'inline-block';
-        Lap_on = 1;
-        return;
-      });
       // 表示更新
-      if (Lap_on){
-        putTime(tDiff - prevDiff);
-       }else{
+      if (Ud_flg==1){
         putTime(tOffset + tDiff);
       }
     },10);
@@ -299,23 +303,58 @@
     // 表示値切替
     tOffset = tOffset + tDiff;
     tDiff = 0;
-    Lap_on = 0;
   });
 
-  buttonElement_exit.addEventListener('click',() => {
-    // ボタン表示切替
-    buttonElement_exit.style.display = 'none';
-    buttonElement_save_exit.style.display = 'none';
-    buttonElement_start.style.display = 'inline-block';
-    buttonElement_reset.style.display = 'inline-block';
+  // STOPボタン押下処理
+  buttonElement_stop.addEventListener('click',()=>{
+      // 繰り返し処理終了
+      clearInterval(timerId);
+      if (Lap_on ==1){
+        putTime(tOffset + tDiff);
+      };
+      Lap_on = 0; // LAPモード終了
+      Set_on = 0; // LAP基準タイムを無効にする
+      // ボタン表示切替
+      buttonElement_stop.style.display = 'none';
+      buttonElement_lap.style.display = 'none';
+      buttonElement_lapstar.style.display = 'none';
+      buttonElement_cont.style.display = 'none';
+      buttonElement_start.style.display = 'inline-block';
+      buttonElement_reset.style.display = 'inline-block';
+      return;
   });
-  buttonElement_save_exit.addEventListener('click',() => {
-    prevDiff = tDiff;
+  // LAPボタン押下処理
+  buttonElement_lap.addEventListener('click',()=>{
+    Ud_flg=0; // 表示更新なし
+    Lap_on = 1; // Lapモード
     // ボタン表示切替
-    buttonElement_exit.style.display = 'none';
-    buttonElement_save_exit.style.display = 'none';
-    buttonElement_start.style.display = 'inline-block';
-    buttonElement_reset.style.display = 'inline-block';
+    buttonElement_lap.style.display = 'none';
+    buttonElement_cont.style.display = 'inline-block';
+    tPrevDiff = tOffset+tDiff;
+    putTime(tPrevDiff);
+    tLapBase = tPrevDiff;
+    Set_on = 1 // LAP基準タイム設定済
+    return;
+  });
+  // LAP STARボタン押下処理
+  buttonElement_lapstar.addEventListener('click',()=>{
+    Ud_flg=0; // 表示更新なし
+    Lap_on = 1; // Lapモード
+    // ボタン表示切替
+    buttonElement_lapstar.style.display = 'none';
+    buttonElement_cont.style.display = 'inline-block';
+    tPrevDiff = tOffset+tDiff;
+    putTime(tPrevDiff-tLapBase);
+    tLapBase = tPrevDiff;
+    return;
+  });
+  // CONTボタン押下処理
+  buttonElement_cont.addEventListener('click',()=>{
+      Ud_flg = 1; // 表示更新　再開
+      // ボタン表示切替
+      buttonElement_cont.style.display = 'none';
+      buttonElement_lapstar.style.display = 'inline-block';
+      return;
   });
   buttonElement_reset.addEventListener('click',() => {
     tOffset = 0;
